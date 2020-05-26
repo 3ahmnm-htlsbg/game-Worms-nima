9b2a61b6a2cd0d8403bc76ff4d7cc581e235fd1b
nima <nina.maringer01@htl-salzburg.ac.at>
2020-05-26 13:59:21 +0200

    update worm controller

--- Assets/MyGame/Scripts/WormController.cs
+++ Assets/MyGame/Scripts/WormController.cs
@@ -5,14 +5,13 @@ using UnityEngine;
 public class WormController : MonoBehaviour
 {
     public KeyCode jumpKey;
-    public KeyCode forwardKey;
-    public KeyCode backwardKey;
+    public KeyCode forwardsKey;
+    public KeyCode backwardsKey;
 
     public Rigidbody rb;
 
     public Vector3 y;
     public Vector3 z;
-    public Vector3 negativez;
     public ForceMode f;
 
 
@@ -22,19 +21,19 @@ public class WormController : MonoBehaviour
         if (Input.GetKeyDown(jumpKey))
         {
             Debug.Log("Die Jump Taste wurde gedrückt");
-            rb.AddForce(y,f);
+            rb.AddForce(y);
         }
 
-        if (Input.GetKeyDown(forwardKey))
+        if (Input.GetKeyDown(forwardsKey))
         {
             Debug.Log("Die Forward Taste wurde gedrückt");
-            rb.AddForce(z, f);
+            rb.AddForce(z);
         }
 
-        if (Input.GetKeyDown(backwardKey))
+        if (Input.GetKeyDown(backwardsKey))
         {
             Debug.Log("Die Backward Taste wurde gedrückt");
-            rb.AddForce(negativez, f);
+            rb.AddForce(-z);
         }
 
     }
